57e3cdb5aa68dd3d1f7e0ab6b0127382f8a60965
HexEnsemble <hexensemble@hotbakedgoods.com>
2026-01-11 21:39:38 +0000

    Refactored load_app_spec()

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::path::PathBuf;
 mod app;
 mod core;
 
+const APP_SPEC_FILENAME: &str = "app-spec.json";
+
 fn main() {
     match load_app_spec() {
         Ok(app_spec) => {
-            println!("Success!");
-
             let mut app = Application::new(app_spec);
 
             app.set_initial_layer(crate::app::initial_layer());
@@ -24,10 +24,10 @@ fn main() {
 fn load_app_spec() -> Result<ApplicationSpecification, Box<dyn std::error::Error>> {
     println!("Loading Application Specification...");
 
-    let path = PathBuf::from("app-spec.json");
+    let path = PathBuf::from(APP_SPEC_FILENAME);
 
     if !path.exists() {
-        println!("Unable to locate app-spec.json, creating defaults...");
+        println!("Unable to locate '{APP_SPEC_FILENAME}', creating defaults...");
 
         let default_spec = ApplicationSpecification {
             title: "Untitled".to_string(),
@@ -35,21 +35,22 @@ fn load_app_spec() -> Result<ApplicationSpecification, Box<dyn std::error::Error
             height: 600,
             fps: 30,
         };
-
         let data = serde_json::to_string_pretty(&default_spec)?;
 
         println!("Done!");
-        println!("Saving app-spec.json...");
+        println!("Saving {APP_SPEC_FILENAME}...");
 
         fs::write(&path, data)?;
 
-        println!("Saved!");
+        println!("Success!");
 
         return Ok(default_spec);
     }
 
     let data = fs::read_to_string(&path)?;
     let app_spec: ApplicationSpecification = serde_json::from_str(&data)?;
+
+    println!("Success!");
 
     Ok(app_spec)
 }
