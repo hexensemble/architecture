4b47013539b30ad20c75dbe7c7e83ed1d5c1d001
HexEnsemble <hexensemble@hotbakedgoods.com>
2026-03-23 21:27:35 +0000

    Implemented dedicated server logging

--- src/bin/dedicated_server.rs
+++ src/bin/dedicated_server.rs
@@ -3,18 +3,52 @@ use architecture::net::protocol::message::*;
 use architecture::net::server_sim::*;
 use architecture::net::stepper::*;
 use bitcode::encode;
+use log::LevelFilter;
 use renet::{ConnectionConfig, DefaultChannel, RenetServer, ServerEvent};
 use renet_netcode::{NetcodeServerTransport, ServerAuthentication, ServerConfig};
+use simplelog::{
+    ColorChoice, CombinedLogger, ConfigBuilder, LevelPadding, TermLogger, TerminalMode, WriteLogger,
+};
 use std::env;
+use std::fs::{self, OpenOptions};
 use std::net::UdpSocket;
+use std::path::PathBuf;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+fn innit_logging() -> Result<(), Box<dyn std::error::Error>> {
+    let log_dir = PathBuf::from("logs");
+    fs::create_dir_all(&log_dir)?;
+    let log_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(log_dir.join("dedicated_server.log"))?;
+
+    let config = ConfigBuilder::new()
+        .set_time_level(LevelFilter::Info)
+        .set_level_padding(LevelPadding::Right)
+        .build();
+
+    let file_logger = WriteLogger::new(LevelFilter::Info, config.clone(), log_file);
+
+    let term_logger = TermLogger::new(
+        LevelFilter::Info,
+        config.clone(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    );
+
+    CombinedLogger::init(vec![file_logger, term_logger])?;
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    innit_logging()?;
+
     let current_time = SystemTime::now().duration_since(UNIX_EPOCH)?;
 
-    //TODO: Needs to be command line argument
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:27960".to_string());
@@ -44,16 +78,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let running_clone = running.clone();
 
     ctrlc::set_handler(move || {
-        println!("[Dedicated Server] Shutting down...");
+        log::info!("[Dedicated Server] Shutting down server...");
         running_clone.store(false, Ordering::SeqCst);
     })?;
 
     // Start server
 
+    log::info!("[Dedicated Server] Starting server...");
+
     let mut sim = ServerSim::default();
     sim.reset();
 
-    println!("[Dedicated Server] Listening on: {}", server_addr);
+    log::info!("[Dedicated Server] Listening on: {}", server_addr);
 
     let fixed_dt = sim.fixed_dt();
     let mut server_stepper = ServerStepper::new(fixed_dt, MAX_STEPS_PER_FRAME);
@@ -67,19 +103,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             server.update(dt);
 
             if let Err(e) = server_transport.update(dt, &mut server) {
-                eprintln!("[Dedicated Server] Server transport update error: {}", e);
+                log::error!("[Dedicated Server] Server transport update error: {}", e);
                 return;
             };
 
             while let Some(event) = server.get_event() {
                 match event {
                     ServerEvent::ClientConnected { client_id } => {
-                        println!("[Dedicated Server] Client connected: {}", client_id);
+                        log::info!("[Dedicated Server] Client connected: {}", client_id);
                     }
                     ServerEvent::ClientDisconnected { client_id, reason } => {
-                        println!(
+                        log::info!(
                             "[Dedicated Server] Client disconnected: {}, {}",
-                            client_id, reason
+                            client_id,
+                            reason
                         );
                     }
                 }
@@ -99,11 +136,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Shutdown server
 
-    println!("[Dedicated Server] Disconnecting clients...");
+    log::info!("[Dedicated Server] Disconnecting clients...");
     server.disconnect_all();
     // Flush packets
     server_transport.send_packets(&mut server);
-    println!("[Dedicated Server] Shutdown complete!");
+    log::info!("[Dedicated Server] Shutdown complete!");
 
     Ok(())
 }
