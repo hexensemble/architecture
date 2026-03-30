8ccb749d083e4ab6b6a8f345ac0d530f1a9a52bd
HexEnsemble <hexensemble@hotbakedgoods.com>
2026-03-30 10:03:06 +0100

    make_client_id() now uses UUID

--- src/net/config.rs
+++ src/net/config.rs
@@ -1,4 +1,4 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use uuid::Uuid;
 
 pub const PROTOCOL_ID: u64 = 0x11223344;
 pub const MAX_STEPS_PER_FRAME: u32 = 8;
@@ -8,8 +8,7 @@ pub const CLIENT_ADDR: &str = "0.0.0.0:0";
 pub const FIXED_DT: f32 = 1.0 / 60.0;
 
 pub fn make_client_id() -> u64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .map(|d| d.as_nanos() as u64)
-        .unwrap_or(0)
+    let id = Uuid::new_v4();
+
+    id.as_u128() as u64
 }
